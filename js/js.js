1abeec29787210dc3003ca381761562d0b5c94e1
Andrei-DD <indus49d@yandex.ru>
2025-02-01 13:46:15 +0300

    dir js, fonts

---
+++ js/js.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    mousewheel: true,
+    speed: 1000,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+//console.log();
